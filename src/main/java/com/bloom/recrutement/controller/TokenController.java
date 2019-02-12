b8ecc4495540d71268705212ca96866a8c730c21
a744849 <chenggang.hu@atos.net>
2019-02-12 10:10:26 +0100

    done: spring security demo todo: split security into front(Vue.js) and back(spring boot)

---
+++ src/main/java/com/bloom/recrutement/controller/TokenController.java
@@ -0,0 +1,27 @@
+package com.bloom.recrutement.controller;
+
+import com.bloom.recrutement.model.JwtUser;
+import com.bloom.recrutement.security.JwtGenerator;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+@Component
+@Path("token")
+public class TokenController {
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String generate(JwtUser jwtUser){
+
+        return jwtGenerator.generate(jwtUser);
+    }
+}
---
+++ src/main/java/com/bloom/recrutement/model/JwtUser.java
@@ -0,0 +1,27 @@
+package com.bloom.recrutement.model;
+
+public class JwtUser {
+    private String userName;
+    private long id;
+    private String role;
+    public void setUsername(String subject) {
+    }
+
+    public void setId(long userId) {
+    }
+
+    public void setRole(String role) {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
